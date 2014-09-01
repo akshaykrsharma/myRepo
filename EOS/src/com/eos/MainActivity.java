2c02d799f4e2482ffbe031206fd30bc0eccd4369
tommullins <tommullins@10.1.1.250>
2014-09-01 05:29:54 -0700

    apk

--- EOS/src/com/eos/MainActivity.java
+++ EOS/src/com/eos/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
     @Override  
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //test 
+        //test 12
         setContentView(R.layout.activity_main);
     }   
 
